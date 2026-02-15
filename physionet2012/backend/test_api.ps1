fa07765c7cd16a8dbdcb1b16e410be0e64c7ed8b
Bernard Swanepoel <104361159+XoXoTheFrozenFox@users.noreply.github.com>
2026-02-15 14:49:22 +0200

    added

--- physionet2012/backend/test_api.ps1
+++ physionet2012/backend/test_api.ps1
@@ -21,8 +21,8 @@ conda run -n MedGemma --no-capture-output python -u .\medgemma_api.py `
 
 conda run -n MedGemma --no-capture-output python -u .\medgemma_api_text.py `
     --base-model "google/medgemma-1.5-4b-it" `
-    --adapter-dir "C:\CODE ON GITHUB\Medgemma4B_physionet2012-2019\physionet2012\results\Qlora\checkpoint-2586" `
-    --processor-dir "C:\CODE ON GITHUB\Medgemma4B_physionet2012-2019\physionet2012\results\Qlora" `
+    --adapter-dir "C:\CODE ON GITHUB\Medgemma4B_physionet2012\physionet2012\results\Qlora\checkpoint-2586" `
+    --processor-dir "C:\CODE ON GITHUB\Medgemma4B_physionet2012\physionet2012\results\Qlora" `
     --use-4bit `
     --device cuda `
     --host 0.0.0.0 `
